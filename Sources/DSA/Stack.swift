890805d721252ea414914ccfd748dccff0313558
Alex Plekhanov <mail@aplekhanov.com>
2023-09-17 09:23:20 -0700

    Add Stack data structure with unit tests.

--- Sources/DSA/Stack.swift
+++ Sources/DSA/Stack.swift
@@ -1,5 +1,28 @@
 import Foundation
 
-public struct Stack {
+/// A stack is a LIFO (Last In, First Out) structure used for storing elements.
+public struct Stack<Element> {
     
+    private var storage: Array<Element> = []
+    
+    public init() {}
+        
+    /// Adds an element to the top.
+    public mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+        
+    /// Removes and returns the top element. Returns nil if empty.
+    @discardableResult
+    public mutating func pop() -> Element? {
+        storage.popLast()
+    }
+    
+    /// Shows the top element without removing it. Returns nil if empty.
+    public func peek() -> Element? {
+        storage.last
+    }
+    
+    /// True if the stack is empty.
+    public var isEmpty: Bool { peek() == nil }
 }
--- Tests/DSATests/StackTests.swift
+++ Tests/DSATests/StackTests.swift
@@ -1,13 +1,37 @@
 import XCTest
+import DSA
 
 final class StackTests: XCTestCase {
 
+    var sut: Stack<Int>!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = Stack()
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
-
+    
+    func testStack_isEmpty() {
+        XCTAssertTrue(sut.isEmpty)
+    }
+    
+    func testStack_push_peek() {
+        sut.push(1)
+        XCTAssertNotNil(sut.peek())
+        XCTAssertEqual(sut.peek()!, 1)
+        XCTAssertFalse(sut.isEmpty)
+    }
+    
+    func testStack_push_pop() {
+        sut.push(1)
+        XCTAssertFalse(sut.isEmpty)
+        let popped = sut.pop()
+        XCTAssertNotNil(popped)
+        XCTAssertEqual(popped, 1)
+        XCTAssertNil(sut.pop())
+        XCTAssertTrue(sut.isEmpty)
+    }
 }
